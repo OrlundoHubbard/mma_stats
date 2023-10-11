9d073d3500cc78cd0be23a2f5ae3ca0ebb836ca3
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-10-10 22:10:31 -0500

    new uploads

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
+
+app.get('/api/mma', async (req, res) => {
+
+    const options = {
+        method: 'GET',
+        url: 'https://mmaapi.p.rapidapi.com/api/mma/search/mcgregor',
+        headers: {
+            'X-RapidAPI-Key': process.env.MMA_KEY,
+            'X-RapidAPI-Host': 'mmaapi.p.rapidapi.com'
+        }
+    };
+
+
+})
